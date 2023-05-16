a15895cf2241bf147056d73eb05aae3b510f71b9
Péter Kalicz <kaliczp@gmail.com>
2023-05-16 17:12:47 +0200

    We replaced the indices with for loop

--- timesermoremonth.R
+++ timesermoremonth.R
@@ -5,12 +5,13 @@ Sys.setlocale("LC_ALL", "C")
 filenames <- dir(pattern = "SoE")
 meteo  <-  as.data.frame(read_excel(path = filenames[1], sheet = 2))
 meteonames <- names(meteo)
-meteo2  <-  data.frame(read_excel(path = filenames[2], sheet = 2))
-names(meteo2) <- meteonames
-meteo3  <-  data.frame(read_excel(path = filenames[3], sheet = 2))
-names(meteo3) <- meteonames
-meteoful <- rbind(meteo, meteo2, meteo3)
 
-soiltemp.xts <- xts(meteoful[,c(10,12,14,16)], meteoful[,1])
+for(ttindex in 2:length(filenames)) {
+    ttmeteo  <-  data.frame(read_excel(path = filenames[ttindex], sheet = 2))
+    names(ttmeteo) <- meteonames
+    meteo <- rbind(meteo, ttmeteo)
+}
+
+soiltemp.xts <- xts(meteo[,c(10,12,14,16)], meteo[,1])
 
 plot(soiltemp.xts)
