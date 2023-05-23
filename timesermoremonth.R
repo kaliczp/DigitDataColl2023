55145edee9885a4e19dd9e1919b51142672aa39f
Péter Kalicz <kaliczp@gmail.com>
2023-05-23 15:51:14 +0200

    Correct the function name in for loop

--- timesermoremonth.R
+++ timesermoremonth.R
@@ -7,7 +7,7 @@ meteo  <-  as.data.frame(read_excel(path = filenames[1], sheet = 2))
 meteonames <- names(meteo)
 
 for(ttindex in 2:length(filenames)) {
-    ttmeteo  <-  data.frame(read_excel(path = filenames[ttindex], sheet = 2))
+    ttmeteo  <-  as.data.frame(read_excel(path = filenames[ttindex], sheet = 2))
     names(ttmeteo) <- meteonames
     meteo <- rbind(meteo, ttmeteo)
 }
