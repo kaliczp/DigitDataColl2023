4ecb3da246e9f9d960d8e8c5e783a35b3503ede7
Péter Kalicz <kaliczp@gmail.com>
2023-10-17 12:46:53 +0200

    Generate summaries and export data

--- airtemp.R
+++ airtemp.R
@@ -1,1 +1,11 @@
 airtempprec.xts <- xts(meteo[,2:6], meteo[,1])
+write.zoo(airtempprec.xts, "rawdata.csv", row.names = F, dec =",")
+
+## Átlagok
+## Boreas állomásból napi csapadékösszeg
+## napi átlaghőmérséklet
+napi <- round(apply.daily(airtempprec.xts[,1], mean),2)
+napicsap <- apply.daily(airtempprec.xts[,3], sum)
+names(napi)
+names(napicsap) <- "Csap"
+write.zoo(cbind(napi, napicsap), "dailydat.csv", row.names = F, dec =",")
